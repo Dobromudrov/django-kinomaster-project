68c2fc8dda9a560f967981c01c686c3bf7c8158b
ArtsemV <adbwork@mail.ru>
2022-05-03 19:51:55 +0300

    add index page with movies

--- kinomaster/main_page/views.py
+++ kinomaster/main_page/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
-
 from movies.models import *
 
 
-# class IndexPage(View):
-#
-#     def get(self, request):
-#         movies = Movies.objects.all()
-#         return render(request, "main_page/index.html", {"movie_list": movies})
+class IndexPage(View):
+    def get(self, request):
+        movies = Movies.objects.all()
+        return render(request, "main_page/index.html", {"movie_list": movies})
